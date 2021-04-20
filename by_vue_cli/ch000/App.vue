62216183f258e20403c95534e3f0946275b8eb0a
sky0621 <sky14work@gmail.com>
2021-04-20 21:27:21 +0900

    del

--- by_vue_cli/ch000/App.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div>
-        <p>Hello, Vue!</p>
-        <h2>aaa</h2>
-    </div>
-</template>
--- old/old_plain/components/index.js
+++
@@ -1,4 +0,0 @@
-// todo-item と呼ばれる新しいコンポーネントを定義
-Vue.component('todo-item', {
-    template: '<li>This is a todo</li>'
-});
