1b1e3b042edec7e655eac1e47e0a1fd882af4d75
sky0621 <sky14work@gmail.com>
2021-05-05 22:59:05 +0900

    re

---
+++ stylelint.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    plugins: ['stylelint-order'],
+    extends: ['stylelint-config-recommended-scss'],
+    rules: {
+      'selector-pseudo-element-no-unknown': [
+        true,
+        {
+          ignorePseudoElements: ['v-deep'],
+        },
+      ],
+      'order/order': ['custom-properties', 'declarations'],
+      'order/properties-alphabetical-order': true,
+    },
+  };
+  
